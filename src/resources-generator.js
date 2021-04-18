43355300cb8a0c624aaac9ee91e52face9deec11
benjamin <benjamin.zamour@gmail.com>
2021-04-18 19:11:06 +0200

    Generate html and css files from scripts

--- src/resources-generator.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs-extra");
-
-exports.install = (outputDir) => {
-    return fs.copy("../resources", outputDir, { overwrite: true })
-        .then(() => {
-            console.log("Resouces installation success")
-        });
-};
