4bc641ad93f574c91340054082d314e5c5eb103e
benjamin <benjamin.zamour@gmail.com>
2021-07-09 11:21:07 +0200

    final commit

--- src/resources-generator.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs-extra");
-
-exports.install = (inputDir, outputDir) => {
-    return fs.copy(inputDir, outputDir, { overwrite: true })
-        .then(() => {
-            console.log("Resouces installation success")
-        });
-};
